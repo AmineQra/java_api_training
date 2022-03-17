9e8999724e08056b33281aa0ac4ad5063377d6b0
amine.ahdadouch <amine.ahdadouch@epita.fr>
2022-03-17 12:31:02 +0100

    Setup Project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java_api_training
+
+![example workflow](https://github.com/Amine-Ahdadouch/java_api_training/actions/workflows/build.yml/badge.svg)
---
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -0,0 +1,7 @@
+package fr.lernejo.navy_battle;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Game Start");
+    }
+}
